21aa5d21cd7f65a23b0a34ca8117c261f8f9cf01
Rcw <rcwilson995@gmail.com>
2017-07-06 14:42:05 +0100

    Added Script test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Found it!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
